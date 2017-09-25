2552a3ac3290f9a489a8574e8a925473e99f0eef
doomsower <K.Kuznetcov@gmail.com>
2017-09-25 11:01:08 +0300

    Pass navigationOptions to createDrilldownScreen

--- src/createDrilldownScreen.tsx
+++ src/createDrilldownScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import { WhitePortal } from 'react-native-portal';
+import { StackNavigatorConfig, TabNavigatorConfig } from 'react-navigation';
 import Icon from './Icon';
 import { CHECK_ICON } from './icons';
 import MultiselectDone from './MultiselectDone';
@@ -16,6 +17,7 @@ export interface DrilldownScreenParams {
 export interface DrilldownScreenOptions {
   HeaderRight?: React.ComponentType;
   screenStyle?: StyleProp<ViewStyle>;
+  navigationOptions?: StackNavigatorConfig & TabNavigatorConfig;
 }
 
 const DefaultHeaderRight = () => <Icon source={CHECK_ICON} />;
@@ -23,6 +25,8 @@ const DefaultHeaderRight = () => <Icon source={CHECK_ICON} />;
 export const createDrilldownScreen = (options: DrilldownScreenOptions = {}) => {
   const {
     HeaderRight = DefaultHeaderRight,
+    screenStyle,
+    navigationOptions,
   } = options;
 
   const DrilldownScreen: StatelessScreen<{}, DrilldownScreenParams> = ({ navigation }) => {
@@ -30,7 +34,7 @@ export const createDrilldownScreen = (options: DrilldownScreenOptions = {}) => {
     const portalName = params ? (params.drilldownItemId || params.rootPortalName) : 'root';
     const rootDrilldownScreenKey = (params && params.rootDrilldownScreenKey) || navigation.state.key;
     return (
-      <View style={[StyleSheet.absoluteFill, options.screenStyle]}>
+      <View style={[StyleSheet.absoluteFill, screenStyle]}>
         <WhitePortal name={`drilldownPortal_${portalName}`} childrenProps={{ rootDrilldownScreenKey }} />
       </View>
     );
@@ -50,6 +54,7 @@ export const createDrilldownScreen = (options: DrilldownScreenOptions = {}) => {
           <HeaderRight />
         </MultiselectDone>
       ) : (null as any),
+      ...navigationOptions,
     };
   };
 
